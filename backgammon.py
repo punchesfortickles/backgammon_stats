15b5e11a2369b8482dd5067ed30e6b05a36d59b3
punchesfortickles <mathewseat@gmail.com>
2017-02-05 21:15:32 -0800

    Update backgammon.py

--- backgammon.py
+++ backgammon.py
@@ -1,43 +1,51 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 18:54:14 2017
+ 
+@author: Nate
+"""
+ 
+ 
 import sys
+import math
 import matplotlib.pyplot as plt
-def master_list(arg, condi):	
-  ary = []
-  length = len(arg)
-  if condi == True:
-	  for i in range(0,length,3):
-	    ary.append([int(arg[i]),int(arg[i+1])])
-	    clean(ary,condi)
-  else:
-    for i in range(0,length,3):
-      ary.append([int(arg[i])])
-      ary.append([int(arg[i+1])])
-      clean(ary,condi)
-  return ary 
-	    
-def clean(arg,condi):
-  if condi == False:
+import matplotlib.mlab as mlab
+import numpy as np
+def master_list(arg):  
+    ary = []
     for i in arg:
-      if i==0:
-        arg.remove(i)
-  elif condi == True:
+        if i!=0.0:    
+            ary.append([int(math.floor(i/10.)),
+                       int(i-10*int(math.floor(i/10.)))])
+    return clean_list(ary)
+ 
+       
+def clean_list(arg):
+    sum_list = []
     for i in arg:
-      if i[0]==0 and i[1]==0:
-        arg.remove(i)
-    return arg
-    
+        if(i[0]<1 or i[1]<1 or i[0]>6 or i[1]>6):
+            arg.remove(i)
+        else:
+            x = int(i[0]+i[1])
+            sum_list.append(x)
+       
+    return sum_list        
+   
 def histogram(arg):
-  plt.hist(arg, bins = 'auto')
-  plt.title("Backgammon")
-  plt.savefig('myfig')
-
-
+    No_Rolls = len(arg)
+    plt.xlim([2,13])
+    x = np.linspace(2,13,No_Rolls)
+    plt.plot(x,mlab.normpdf(x,np.average(arg),np.std(arg)))
+    plt.hist(arg,bins = [2,3,4,5,6,7,8,9,10,11,12,13],normed =1)
+    plt.title("Backgammon")
+    plt.grid(True)
+    plt.ylabel("# of rolls = {}".format(len(arg)))  
+    plt.xlabel('Sum of roll')
+    plt.savefig('myfig')
+ 
 if __name__ == "__main__":
-  with open(sys.argv[1]) as f:
-    content = f.readlines()
-  content = [str(x) for x in content]
-  histogram(master_list(content[0],False))
-  
-    
-    
-  
+    with open(sys.argv[1]) as f:
+        content = f.readlines()
+    contents = [float(x.strip()) for x in content]
+    #print(master_list(contents))
+    histogram(master_list(contents))
